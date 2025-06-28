9c50d2cca93fd4414a7613fe4e8b74486c908edc
mounir <mounir.hammami@univ-tlse3.fr>
2025-06-29 01:08:29 +0200

    la somme

---
+++ somme.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int a = 5,b = 8;
+    int somme =  a+b;
+    printf("La somme de %d et %d est %d\n", a, b, somme);
+    return 0;
+}
